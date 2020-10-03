475c1d8354362b3c589028ca0b24b509229b4857
mpatel18 <meekitpatel@gmail.com>
2020-10-03 18:50:01 -0400

    overwite vue

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
-    <Header />
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    HelloWorld
   }
 }
 </script>
--- site/src/components/Header.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h2>Build</h2>
-</template>
