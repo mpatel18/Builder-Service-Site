4652692682a7d96a9eb61b22672994ac1ea5eaa8
mpatel18 <meekitpatel@gmail.com>
2020-10-04 14:29:02 -0400

    updated description

--- site/src/components/Description.vue
+++ site/src/components/Description.vue
@@ -4,14 +4,14 @@
             <template v-slot:header>Build.Net</template>
 
             <template v-slot:lead>
-                Your one stop shop to design, build, and customize your service of 
+                Your one template to design, build, and customize your service of 
                 tomorrow, today. 
             </template>
 
             <hr class="my-4">
 
             <p>
-                A template designed to get you up and running.
+                Designed to get you up and running.
             </p>
         </b-jumbotron>
     </div>
