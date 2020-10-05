43ec31f0b720ae7b4b5483be2f8bf3b8b18afe3c
mpatel18 <meekitpatel@gmail.com>
2020-10-05 19:22:20 -0400

    added vuetify

--- site/src/main.js
+++ site/src/main.js
@@ -5,11 +5,13 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify';
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
