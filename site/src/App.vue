5ecf8d7b319136e98af7bbe29f6d8570ec3bd715
mpatel18 <meekitpatel@gmail.com>
2020-10-05 23:20:31 -0400

    remove useless components

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,7 +1,60 @@
 <template>
   <div id="app">
-    <Header />
-    <Description class ="test" />
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand href="#services">Build.net</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="#">Services</b-nav-item>
+                    <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+                    <!-- <b-nav-form> 
+                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                    </b-nav-form> -->
+
+                    <b-nav-item-dropdown text="Lang" right disabled>
+                        <b-dropdown-item href="#">EN</b-dropdown-item>
+                        <b-dropdown-item href="#">ES</b-dropdown-item>
+                        <b-dropdown-item href="#">RU</b-dropdown-item>
+                        <b-dropdown-item href="#">FA</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                    <b-nav-item-dropdown right disabled>
+                        <!-- Using 'button-content' slot -->
+                        <template v-slot:button-content>
+                            <em>User</em>
+                        </template>
+                        <b-dropdown-item href="#">Profile</b-dropdown-item>
+                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+    <div>
+        <b-jumbotron bg-variant="secondary" text-variant="light" border-variant="dark">
+            <template v-slot:header>Build.Net</template>
+
+            <template v-slot:lead>
+                Your one template to design, build, and customize your service of 
+                tomorrow, today. 
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+                Designed to get you up and running.
+            </p>
+        </b-jumbotron>
+    </div>
     <div class ="services">
       <section class="offers">
         <Services 
@@ -37,16 +90,12 @@
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Description from './components/Description.vue'
 import Services from './components/Services.vue'
 import Reviews from './components/Reviews.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
-    Description,
     Services,
     Reviews
   },
@@ -137,8 +186,8 @@ export default {
   }
 
   .user-response {
-    padding-top: 2%;
-    padding-left: 12%;
+    padding-top: 3%;
+    padding-left: 10%;
     padding-bottom: 2%;
   }
 
