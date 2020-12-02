3fe88a32c36450d4cdb51b4cc0a759ed8cca4e1b
mpatel18 <meekitpatel@gmail.com>
2020-12-02 18:39:24 -0500

    rework app structure

--- site/src/App.vue
+++ site/src/App.vue
@@ -3,27 +3,17 @@
     <div>
         <b-navbar toggleable="lg" type="dark" variant="dark">
             <b-navbar-brand href="#">Build.net</b-navbar-brand>
-
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
                     <b-nav-item href="#review">{{ $t('review_tag') }}</b-nav-item>
-                    <!-- <b-nav-item href="#" disabled>Disabled</b-nav-item> -->
                 </b-navbar-nav>
 
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
-                    <!-- <b-nav-form> 
-                        <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                    </b-nav-form> -->
-
                     <b-nav-item-dropdown text="Lang" right>
                         <b-dropdown-item @click="changeLocale('en')">EN</b-dropdown-item>
                         <b-dropdown-item @click="changeLocale('es')">ES</b-dropdown-item>
-                        <!-- <b-dropdown-item>RU</b-dropdown-item>
-                        <b-dropdown-item>FA</b-dropdown-item> -->
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
             </b-collapse>
@@ -198,5 +188,4 @@ export default {
     padding-left: 15%;
     padding-bottom: 2%;
   }
-
 </style>
