f63c4e444f432091995d67082313e14cceb23a8e
mpatel18 <meekitpatel@gmail.com>
2020-12-02 16:42:25 -0500

    removed vuetify

--- site/src/main.js
+++ site/src/main.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import i18n from './plugins/i18n';
 
@@ -8,15 +6,12 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import vuetify from './plugins/vuetify';
 
-Vue.use(Vuetify)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
   i18n,
   render: h => h(App)
 }).$mount('#app')
