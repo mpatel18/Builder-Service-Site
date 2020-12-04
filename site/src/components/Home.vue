2cd7958e600b7c8aa3af38d1e1ce23f5c16176d6
mpatel18 <meekitpatel@gmail.com>
2020-12-04 17:43:56 -0500

    updated home view

--- site/src/components/Home.vue
+++ site/src/components/Home.vue
@@ -2,7 +2,7 @@
     <div>
         <div>
         <b-navbar toggleable="lg" type="dark" variant="dark">
-            <b-navbar-brand href="#Home">Build.net</b-navbar-brand>
+            <b-navbar-brand href="#Home">Logo</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
@@ -20,14 +20,25 @@
         </b-navbar>
         </div>
         <div>
-            <b-jumbotron header="Build.net">
-            <template #lead>{{ $t('descrip') }}</template>
+            <b-jumbotron header="Service">
+            <template #lead>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A placeat, cumque sed 
+                in aliquid sint. Architecto aperiam illo nostrum exercitationem, eveniet 
+                debitis reprehenderit atque quae temporibus quos reiciendis. Nam, optio.
+            </template>
                 <hr class="my-4">
-                <p>{{ $t('tag') }}</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Ducimus nam nemo doloribus.
+                </p>
             </b-jumbotron>
         </div>
-        <h2 class="service_header">{{ $t('s_title') }}</h2>
-        <h2 id ="Review" class ="review-header">{{ $t('review_tag') }}</h2>
+        <div class="service">
+            <h2 class="service_header">{{ $t('s_title') }}</h2>
+        </div>
+        <div class="review">
+            <h2 id ="Review" class ="review-header">{{ $t('review_tag') }}</h2>
+        </div>
     </div>
 </template>
 
