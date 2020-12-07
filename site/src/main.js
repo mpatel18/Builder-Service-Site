d1150c0108b32a4dcbd9a87727832f7f6e0d9cca
mpatel18 <meekitpatel@gmail.com>
2020-12-07 08:37:17 -0500

    add vue router

--- site/src/main.js
+++ site/src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 import i18n from './plugins/i18n';
 
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
 
 new Vue({
   i18n,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
