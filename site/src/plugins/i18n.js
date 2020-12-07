3bfb27181b2acd525372932dec3970c11e54830e
mpatel18 <meekitpatel@gmail.com>
2020-12-07 10:37:53 -0500

    reworked translations file

--- site/src/plugins/i18n.js
+++ site/src/plugins/i18n.js
@@ -3,16 +3,9 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
-const messages = {'en': {review_tag: 'Reviews', 
-                         s_title: 'What We Offer', 
-                        },
-                  'es': {review_tag: 'Críticas', 
-                         s_title: 'Lo que ofrecemos',
-                        }};
-                        
-export default new VueI18n({locale: 'en', // set locale 
-                          fallbackLocale: 'es', // set fallback 
-                          messages // set locale messages
-                        });
+export default new VueI18n({
+  locale: 'en', // set locale 
+  fallbackLocale: 'es', // set fallback 
+});
 
         
