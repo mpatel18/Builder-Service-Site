f6dffc89a9af2604da60179efd1ee317bc0a4356
mpatel18 <meekitpatel@gmail.com>
2020-12-06 23:06:06 -0500

    updated home component

--- site/src/components/Home.vue
+++ site/src/components/Home.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        <div class="service">
+        <div class="nav-container">
+            <header>
+                logo
+            </header>
+            <nav class="navbar">
+            <ul>
+                <li>about</li>
+                <li>service</li>
+                <li>home</li>
+            </ul>
+        </nav>
+        </div>
+        <h1>A tagline for your service</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h2>
+        <div>
+            <div class="block1"></div>
+            <div class="block2"></div>
+        </div>
+        <!-- <div class="service">
             <h2 class="service_header" style="color: white;">{{ $t('s_title') }}</h2>
         </div>
         <div class="review">
             <h2 id ="Review" class ="review-header" style="color: white;">{{ $t('review_tag') }}</h2>
-        </div>
+        </div> -->
     </div>
 </template>
 
@@ -25,5 +43,21 @@ export default {
 <style>
     * {
         background: #1f1f1f;
+    }
+
+    .navbar, header { 
+        color: white; 
+    }
+
+    .block1 { 
+        width: 500px;
+        height: 500px; 
+        background:rgb(143, 46, 46); 
+    }
+
+    .block2 {
+        width: 500px;
+        height: 500px; 
+        background: purple;
     }
 </style>
