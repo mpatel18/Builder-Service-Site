043012bc4c1df646a4f7632e7ec3ef13b067f823
mpatel18 <meekitpatel@gmail.com>
2020-12-16 17:52:23 -0500

    updated service page

--- site/src/components/Splitscreen.vue
+++ site/src/components/Splitscreen.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="split-screen">
       <div class="side1">
-          <button>What we offer</button>
+          <button>What We Offer</button>
       </div>
       <div class="side2">
-          <button>What our users think</button>
+          <button>What Our Users Think</button>
       </div>
   </div>
 </template>
@@ -22,6 +22,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+
     .split-screen {
         display: flex;
         justify-content: space-around;
@@ -37,15 +39,21 @@ export default {
         button { 
             background: transparent;
             border-color: black;
+            transition: border-radius ease 1.5s;
             color: black;
             font-weight: 600;
             font-size: 20px;
+            font-family: 'Nunito', sans-serif;
             box-shadow: none;
             border-width: 10px 10px 10px 10px;
             border-style: solid; 
             outline: none; 
             padding: 4em;
             cursor: pointer;
+        }
+
+        button:hover {
+            border-radius: 3em;
         }
     }
 </style>
