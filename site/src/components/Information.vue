8ae17f5cd8bd2dd340dcf6ddb0ffaa84cf1d02be
mpatel18 <meekitpatel@gmail.com>
2020-12-19 20:21:50 -0500

    updated contact view

--- site/src/components/Information.vue
+++ site/src/components/Information.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="information">
-      Our Contact Info
+      <div class="info-form">
+          <h2>Contact Us</h2>
+          <form action="">
+              <label for="fname">First Name</label>
+              <input type="text" id="fname">
+
+              <label for="lname">Last Name</label>
+              <input type="text" id="lname">
+              
+              <label for="email">Email</label>
+              <input type="email" id="email">
+
+              <label for="message">Message</label>
+              <textarea name="message" id="message" cols="30" rows="10"></textarea>
+
+          </form>
+      </div>
   </div>
 </template>
 
@@ -18,5 +34,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .information {
+        background-color: #443EAD;
+        height: 100vh;
+    }
 
+    .info-form {
+        background-color: #6B5ED2;
+        
+        input[type=text], input[type=email], textarea {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+    }
 </style>
