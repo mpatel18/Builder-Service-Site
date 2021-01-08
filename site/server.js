11bcd93ef0c29a89920255a900d0ffdf06bf63fd
mpatel18 <meekitpatel@gmail.com>
2021-01-08 15:52:45 -0500

    added router history, updated server script

--- site/server.js
+++ site/server.js
@@ -1,13 +1,13 @@
+var history = require('connect-history-api-fallback');
+
 const express = require('express');
 const serve_static = require('serve-static');
 const path = require('path');
 
 const app = express(); 
 
+app.use(history()); 
 app.use('/', serve_static(path.join(__dirname, '/dist'))); 
-app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname, + '/dist/index.html'); 
-});
 
 const PORT = process.env.PORT || 8080; 
 app.listen(PORT); 
