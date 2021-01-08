efd26f818cbe90964dc69a806a289e17f35ad558
mpatel18 <meekitpatel@gmail.com>
2021-01-08 15:10:01 -0500

    removed production build

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const serve_static = requre('serve-static');
-const path = require('path'); 
+// const express = require('express');
+// const serve_static = requre('serve-static');
+// const path = require('path'); 
 
-const app = express(); 
+// const app = express(); 
 
-app.use('/', serve_static(path.join(__dirname, '/dist'))); 
+// app.use('/', serve_static(path.join(__dirname, '/dist'))); 
 
-const PORT = process.env.PORT || 8080; 
-app.listen(PORT); 
+// const PORT = process.env.PORT || 8080; 
+// app.listen(PORT); 
